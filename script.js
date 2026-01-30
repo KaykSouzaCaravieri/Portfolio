2df30425583f7bb86121a7c65ece2a9887ba5207
kayk Caravieri <kayk.caravieri.123@gmail.com>
2026-01-30 17:14:00 -0300

    script

---
+++ script.js
@@ -0,0 +1,16 @@
+function enviarWhats(event){
+    event.preventDefault()
+
+    const nome = document.getElementById('nome').value;
+    const mensagem = document.getElementById('mensagem').value;
+    const telefone = '5518996492963'
+
+    const texto = `Olá! Me chamo ${nome}, ${mensagem}`
+    const msgFormatada = encodeURIComponent(texto)
+
+    const url = `https://wa.me/${telefone}?text=${msgFormatada}`
+
+    console.log(url)
+
+    window.open(url, 'blank')
+}
